9f874792834a74a7ea1781f4e73d6bc843737aca
alankalbermatter <alan.kalbermatter.dev@gmail.com>
2025-08-16 19:28:44 -0300

    fix health check issue

--- src/main/java/com/akalber/alankalbermatter/AlanKalbermatterApplication.java
+++ src/main/java/com/akalber/alankalbermatter/AlanKalbermatterApplication.java
@@ -3,8 +3,12 @@ package com.akalber.alankalbermatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsAutoConfiguration;
 
-@SpringBootApplication(exclude = {SystemMetricsAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    SystemMetricsAutoConfiguration.class,
+    MetricsAutoConfiguration.class
+})
 public class AlanKalbermatterApplication {
 
     public static void main(String[] args) {
